f89530b936e7ed6b6dcd0c2d10a21f8c2fdbc73d
Amjad <sammanamjad86@gmail.com>
2020-04-23 15:48:12 +0200

    add

---
+++ counter/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css'
+
+function Counter() {
+ 
+  const [count, setCount] = useState();
+
+  return (
+    <div className="App">
+      <div >
+      <br/>
+        <form onChange={e => setCount(e.target.value)}>
+          <label for="counter">Number :</label>
+          <input type="number" id="counter" name="counter"/>
+        </form>
+      </div>
+      
+      <div>
+      <br/>
+        <button onClick={() => setCount(count - 1)} className="ui basic red button">
+          -1
+        </button>
+        <button onClick={() => setCount(count - (-1))} className="ui basic green button">
+          +1
+        </button>
+      </div>
+      <br/>
+      <p>Le compteur est  à : {count} </p>
+    </div>
+  );
+}
+
+export default Counter;
+
